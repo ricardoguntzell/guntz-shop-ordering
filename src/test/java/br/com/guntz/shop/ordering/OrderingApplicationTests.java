4b319797d4f08d2f34d62938576c971bfef47419
guntz <rricardoguntzell@gmail.com>
2025-08-13 12:04:40 -0300

    feat: inclusão de entidade do dominio Customer com camada de Testes iniciais

---
+++ src/test/java/br/com/guntz/shop/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guntz.shop.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
